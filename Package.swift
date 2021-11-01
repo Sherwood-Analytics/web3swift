d883ac439ebbec96cede635b8b2df0a156c50062
Ralph Kuepper <ralph.kuepper@skelpo.com>
2021-11-02 00:56:19 +0100

    deps

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
         .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.16.1"),
-        .package(url: "https://github.com/Sherwood-Analytics/Starscream", from: "4.1.2"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.3"),
+        .package(url: "https://github.com/Sherwood-Analytics/Starscream", from: "4.1.3"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
     ],
     targets: [
         .target(name: "web3swift", dependencies: [
