6d55c474d4009d10d0d719a50916162b418b2c8d
Ralph Kuepper <ralph.kuepper@skelpo.com>
2021-11-01 20:46:42 +0100

    cleaned up package

--- Package.swift
+++ Package.swift
@@ -14,20 +14,22 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.15.3"),
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.4"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.3.0"),
+        .package(url: "https://github.com/mxcl/PromiseKit.git", from: "6.16.1"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.2"),
     ],
     targets: [
+        .target(name: "web3swift", dependencies: [
+            .product(name: "BigInt", package: "BigInt"),
+            .product(name: "PromiseKit", package: "PromiseKit"),
+            .product(name: "Starscream", package: "Starscream"),
+            .product(name: "CryptoSwift", package: "CryptoSwift"),
+            .byName(name: "secp256k1")
+        ]),
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "secp256k1"),
-        .target(
-            name: "web3swift",
-            dependencies: ["BigInt", "secp256k1", "PromiseKit", "Starscream", "CryptoSwift"],
-            exclude: [
-            ]),
         .testTarget(
             name: "web3swiftTests",
             dependencies: ["web3swift"]),
